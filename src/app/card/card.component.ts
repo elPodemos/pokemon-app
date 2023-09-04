a77d4004fc1763c7065f2f300fb512ad1fa27a93
PODER Elias <elias.poderr@gmail.com>
2023-09-04 14:06:57 +0200

    a

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Pokemon } from '../pokemon';
 import { POKEMONS } from '../mock-pokemons-list';
+import { PokemonService } from '../pokemon.service';
 
 @Component({
   selector: 'app-card',
@@ -8,25 +9,43 @@ import { POKEMONS } from '../mock-pokemons-list';
 })
 export class CardComponent {
 
+  constructor(
+    private service:PokemonService,
+  ){}
+
   title: string = 'pokemon-app';
   pokemonList: Pokemon[] = POKEMONS;
+  pokemonListService:Pokemon[]= [];
+  pokemonService:Pokemon;
   index: number = Math.floor(Math.random() * 12);
+
+  getPokemon(){
+    this.pokemonListService = this.service.fetchAllPokemon();
+    return this.pokemonListService;
+  }
+
+  getPokemonById(i:number){
+    this.pokemonService = this.service.fetchById(i);
+    return this.pokemonService;
+  }
 
   ngOnInit(): void {
     console.log("C'est moi wsh");
     console.table(this.pokemonList);
+    console.log(this.getPokemonById(0).name);
   }
 
+
   selectPokemon(form: number): void{
-    if(form > this.pokemonList.length || form < 1){
+    if(form > this.getPokemon().length || form < 1){
       console.log("Il n existe pas !");
     }else{
-      console.log("Vous avez selectionner : "+ this.pokemonList[form-1].name);
+      console.log("Vous avez selectionner : "+ this.getPokemon()[form-1].name);
     }
   }
 
   cliquePokemon(id:number):void{
-    console.log("Vous avez selectionner : "+ this.pokemonList[id-1].name);
+    console.log("Vous avez selectionner : "+ this.getPokemonById(id-1).name);
   }
 
 
---
+++ src/app/pokemon.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { POKEMONS } from './mock-pokemons-list';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonService {
+
+  constructor() { }
+
+  fetchAllPokemon(){
+    return POKEMONS;
+  }
+
+  fetchById(i:number){
+    return POKEMONS[i];
+  }
+
+}
